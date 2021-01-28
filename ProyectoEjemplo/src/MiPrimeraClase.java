a45dd8b170fe81f7b59836b4ba02fafaf632c795
Daniela Torres <danielatorres331@hotmail.com>
2021-01-28 14:54:15 +0100

    Modificación en la rama experimental

--- ProyectoEjemplo/src/MiPrimeraClase.java
+++ ProyectoEjemplo/src/MiPrimeraClase.java
@@ -2,7 +2,7 @@
 public class MiPrimeraClase {
 
 	public static void main(String[] args) {
-		System.out.println ("Hola, esto es un nuevo mensaje");
+		System.out.println("Hola, esto es un nuevo mensaje de la nueva rama");
 
 	}
 
